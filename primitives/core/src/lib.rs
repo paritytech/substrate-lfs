971e463a0f5ea29fa1d13255f9bce79c739a004a
Benjamin Kampmann <ben@gnunicorn.org>
2020-01-07 14:32:49 +0100

    implement generic callback flow

--- primitives/core/src/lib.rs
+++ primitives/core/src/lib.rs
@@ -1,16 +1,11 @@
-pub use codec::{Decode, Encode};
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use codec::{Decode, Encode, EncodeLike};
 use core::hash::Hash;
-use sp_std::prelude::*;
+use sp_std::{fmt::Debug, prelude::*};
 
-/// Represent a Large File System Id
-pub trait LfsId: Encode + Decode + Hash + Eq + Clone{
+/// Represent a Large File System Reference
+pub trait LfsId: Encode + EncodeLike + Debug + Decode + Hash + Eq + Clone {
+	/// Generate the LfsId for the given data
 	fn for_data(data: &Vec<u8>) -> Result<Self, ()>;
-}
-
-#[cfg(test)]
-mod tests {
-	#[test]
-	fn it_works() {
-		assert_eq!(2 + 2, 4);
-	}
 }
